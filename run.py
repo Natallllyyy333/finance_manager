d6f8819d3cfc1f26d84c4489ad518f76bbf24602
Natallllyyy333 <natalllyyy333@gmail.com>
2025-08-12 01:38:16 +0200

    create a MONTH file variables and open and read file and print it

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+import csv
+MONTH = input(
+    "Enter the month (e.g., 'March', 'April', 'May'): ").strip().lower()
+
+file = f"hsbc_{MONTH}.csv"
+
+with open(file, mode='r') as csv_file:
+    csv_reader = csv.reader(csv_file)
+    for row in csv_reader:
+        print(row)
