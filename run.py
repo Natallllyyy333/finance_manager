10f953a71054c4fa7de6a3883df2f1fd53ad1560
Natallllyyy333 <natalllyyy333@gmail.com>
2025-08-12 04:02:17 +0200

    insert data rows into googlesheets

--- run.py
+++ run.py
@@ -1,20 +1,36 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 import csv
 import gspread
+import time
 MONTH = input(
     "Enter the month (e.g., 'March', 'April', 'May'): ").strip().lower()
 
 file = f"hsbc_{MONTH}.csv"
 
-with open(file, mode='r') as csv_file:
-    csv_reader = csv.reader(csv_file)
-    for row in csv_reader:
-        date = row[0]
-        name = row[1]
-        amount = float(row[2])
-        category = 'other'
-        transaction = ((date, name, amount, category))
-        print(transaction)
+
+def hsbcFin(file):
+    with open(file, mode='r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        transactions = []
+        for row in csv_reader:
+            date = row[0]
+            name = row[1]
+            amount = float(row[2])
+            category = 'other'
+            if name == 'Monthly Rent Payment':
+                category = 'rent'
+            elif name == 'Gym Membership':
+                category = 'gym'
+            transaction = ((date, name, amount, category))
+
+            transactions.append(transaction)
+        return transactions
+
 
 sa = gspread.service_account(filename='creds.json')
 sh = sa.open("Personal Finances")
+wks = sh.worksheet(f"{MONTH}")
+rows = hsbcFin(file)
+for row in rows:
+    wks.insert_row([row[0], row[1], row[2], row[3]], 8)
+    time.sleep(2)  # Sleep to avoid hitting API limits
