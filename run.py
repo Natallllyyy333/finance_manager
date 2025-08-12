f8622e5562d412f07b4b388ee791b412348a48fa
Natallllyyy333 <natalllyyy333@gmail.com>
2025-08-12 03:16:18 +0200

    add rows date, name, amount, create a variable transaction and print it, edit csv files

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 import csv
+import gspread
 MONTH = input(
     "Enter the month (e.g., 'March', 'April', 'May'): ").strip().lower()
 
@@ -8,4 +9,12 @@ file = f"hsbc_{MONTH}.csv"
 with open(file, mode='r') as csv_file:
     csv_reader = csv.reader(csv_file)
     for row in csv_reader:
-        print(row)
+        date = row[0]
+        name = row[1]
+        amount = float(row[2])
+        category = 'other'
+        transaction = ((date, name, amount, category))
+        print(transaction)
+
+sa = gspread.service_account(filename='creds.json')
+sh = sa.open("Personal Finances")
