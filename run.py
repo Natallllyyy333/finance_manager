fab7948223627edbb83b3c823097899d4f8f9e54
Natallllyyy333 <natalllyyy333@gmail.com>
2025-08-31 19:36:31 +0200

    edit teminal

--- run.py
+++ run.py
@@ -1444,7 +1444,7 @@ if "DYNO" in os.environ:
             font-size: 14px;
             line-height: 1.4;
             overflow: auto;
-            
+            white-space: pre;
             max-height: 400px;
             border: 2px solid #333;
             margin-bottom: 20px;
